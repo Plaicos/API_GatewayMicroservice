441be826518332ad2e14cea43bc5133df26ce6b5
RodrigoHBD <rodrigojacques16@gmail.com>
2020-03-04 00:55:37 -0300

    foo

--- src/config/Express/routes/default/pages/pages_routes.js
+++ src/config/Express/routes/default/pages/pages_routes.js
@@ -5,6 +5,9 @@ module.exports = (newrouter, dependencies) => {
     router.use(/^\/$/, Controller.view_static_public_page("home"))
     router.use("/home", Controller.view_static_public_page("home"))
     router.use("/sign-up", Controller.view_static_public_page("sign_up"))
+    router.use("/log-in", Controller.view_static_public_page("login"))
+    router.use("/sign-up/supplier", Controller.view_static_public_page("sign_up_supplier"))
+    router.use("/sign-up/industry", Controller.view_static_public_page("sign_up_industry"))
 
     //router.use(Controller.handle404())
 
